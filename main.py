2e21fa75a0a23b2a7ee7be7b30de075c27482e63
MK <masupallikushwanth2005@gmail.com>
2026-05-26 16:11:02 +0530

    ready for deployement

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from router import router
+import os
 
 app = FastAPI()
 
+# Serve files inside public folder
+app.mount("/public", StaticFiles(directory="public"), name="public")
+
 @app.get("/")
 def home():
-    return {"message": "server is running"}
+    return FileResponse("public/vectorbase.html")
 
 @app.get("/health")
 def health():
